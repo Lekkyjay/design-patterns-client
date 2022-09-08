b61a92e921040cea7250195c4f5cb9f498c031ab
Lekkyjay <lekkyjay@yahoo.com>
2022-09-08 15:31:55 +0200

    UncontrolledOnboardingFlow works.

--- src/05-onboardingFlows/App05.tsx
+++ src/05-onboardingFlows/App05.tsx
@@ -1,33 +1,40 @@
 import UncontrolledOnboardingFlow from './UncontrolledOnboardingFlow'
 
+type goToNextArgType = {
+  [n: string]: unknown
+}
+
 interface IStep {
-  goToNext?: () => void
+  goToNext?: (x: goToNextArgType) => void
 }
 
 const StepOne = ({goToNext}: IStep) => (
   <>
     <h1>Step 1</h1>
-    <button onClick={goToNext}>Next</button>
+    <button onClick={()=>goToNext?.({name: 'John Doe'})}>Next</button>
   </>
 )
 
 const StepTwo = ({goToNext}: IStep) => (
   <>
     <h1>Step 2</h1>
-    <button onClick={goToNext}>Next</button>
+    <button onClick={()=>goToNext?.({age: 100})}>Next</button>
   </>
 )
 
 const StepThree = ({goToNext}: IStep) => (
   <>
     <h1>Step 3</h1>
-    <button onClick={goToNext}>Next</button>
+    <button onClick={()=>goToNext?.({hairColor: 'brown'})}>Next</button>
   </>
 )
 
 export default function App05() {
   return (
-    <UncontrolledOnboardingFlow>
+    <UncontrolledOnboardingFlow onFinish={data => {
+      console.log('data: ', data)
+      alert('Onboarding completed')
+    }}>
       <StepOne />
       <StepTwo />
       <StepThree />
--- src/05-onboardingFlows/UncontrolledOnboardingFlow.tsx
+++ src/05-onboardingFlows/UncontrolledOnboardingFlow.tsx
@@ -2,19 +2,35 @@ import React, { ReactElement, ReactNode, useState } from 'react'
 
 interface IProps {
   children: ReactNode
-  onFinish?: () => void
+  onFinish?: ({}) => void
+}
+
+type goToNextArgType = {
+  [n: string]: unknown
 }
 
 interface IStep {
-  goToNext: () => void
+  goToNext: (x: goToNextArgType) => void
 }
 
-export default function UncontrolledOnboardingFlow({ children }: IProps) {
+export default function UncontrolledOnboardingFlow({ children, onFinish }: IProps) {
   const [onboardingData, setOnboardingData] = useState({})
   const [currentIndex, setCurrentIndex] = useState(0)
   
-  const goToNext = () => {
-    setCurrentIndex(currentIndex + 1)
+  const goToNext = (stepData: goToNextArgType) => {
+    const nextIndex = currentIndex + 1
+
+    const updatedData = { ...onboardingData, ...stepData }
+
+    console.log('updatedData: ', updatedData)
+    
+    if (nextIndex < React.Children.toArray(children).length) {
+      setCurrentIndex(nextIndex)
+    } else {
+      onFinish?.(updatedData)
+    }
+
+    setOnboardingData(updatedData)
   }
   
   const currentChild = React.Children.toArray(children)[currentIndex] as ReactElement<IStep>
